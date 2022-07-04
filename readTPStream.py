0eec2a50a6699960b69639b0a346a94caf1b6dfe
ShyamB <bhuller.shyam@gmail.com>
2022-07-04 16:14:13 +0000

    Use new Utilities module

--- readTPStream.py
+++ readTPStream.py
@@ -5,6 +5,8 @@ Author: Shyam Bhuller
 
 Description: Plots data from TP stream files from VD coldbox runs
 """
+import Utilities
+
 import detdataformats.trigger_primitive
 from hdf5libs import HDF5RawDataFile
 import daqdataformats
@@ -17,28 +19,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import pandas as pd
-
-def LogFragmentData(debug, fragment_path, fragment):
-    """ Log some fragment data
-
-    Args:
-        debug (bool): should we log
-        fragment_path (str): fragment path
-        fragment (daqdataformats.fragment): fragment
-    """
-    if debug:
-        print(fragment_path)
-        print(fragment)
-        print(f"Inspecting {fragment_path}")
-        print(f"Run number : {fragment.get_run_number()}")
-        print(f"Trigger number : {fragment.get_trigger_number()}")
-        print(f"Trigger TS    : {fragment.get_trigger_timestamp()}")
-        print(f"Window begin  : {fragment.get_window_begin()}")
-        print(f"Window end    : {fragment.get_window_end()}")
-        print(f"Fragment type : {fragment.get_fragment_type()}")
-        print(f"Fragment code : {fragment.get_fragment_type_code()}")
-        print(f"Size          : {fragment.get_size()}")
-
 
 def LogTPData(debug, tp):
     """ Log some tp data
@@ -80,38 +60,8 @@ def TPDataFrame() -> dict:
     }
 
 
-def SortDataByPlane(data : pd.DataFrame) -> dict:
-    """ Split data frame by which plane the channels correspond to.
-
-    Args:
-        data (pd.DataFrame): data which contains offline channel numbers
-
-    Returns:
-        dictionary: data frames corresponding to each plane
-    """
-    uPlane = data[data["plane"] == 0] # induction
-    vPlane = data[data["plane"] == 1] # induction
-    zPlane = data[data["plane"] == 2] # collection
-    return {"u": uPlane, "v": vPlane, "z": zPlane}
-
-
 def main(args):
-    h5_file = HDF5RawDataFile(args.file)
-    cmap = detchannelmaps.make_map('VDColdboxChannelMap') #? make channel map configurable?
-
-    #* get attributes using h5py
-    h5py_file = h5py.File(args.file, 'r')
-    for attr in h5py_file.attrs.items():
-        print("File Attribute ", attr[0], " = ", attr[1])
-
-    records = h5_file.get_all_record_ids()
-    print("Number of records: %d" % len(records))
-
-    #? allow possiblity to select multiple time slices rather than one or all
-    if(args.timeslice > -1):
-        toStudy = [records[args.timeslice]]
-    else:
-        toStudy = records
+    h5_file, cmap, toStudy = Utilities.OpenFile(args.file, args.records)
 
     TPData = []
     for rid in toStudy:
@@ -119,7 +69,7 @@ def main(args):
 
         fragment_path = h5_file.get_fragment_dataset_paths(rid)[0] # get the path to the data
         fragment = h5_file.get_frag(fragment_path)
-        LogFragmentData(args.debug, fragment_path, fragment)
+        Utilities.LogFragmentData(args.debug, fragment_path, fragment)
 
         #* get number of TPs in fragment
         tp_size = detdataformats.trigger_primitive.TriggerPrimitive.sizeof() # get size of TP packet in bytes
@@ -160,7 +110,7 @@ def main(args):
         anim.save(f"{args.outdir}/anim.gif")
 
     if args.plot == "scatter" and args.timeslice > -1:
-        planes = SortDataByPlane(TPData[0])
+        planes = Utilities.SortDataByPlane(TPData[0])
 
         for p in planes:
             plt.figure()
@@ -169,9 +119,10 @@ def main(args):
             cbar.set_label("ADC integral")
             plt.xlabel("channel")
             plt.ylabel("peak time")
-            plt.title(f"Time sice :{records[args.timeslice]}, Plane : {p}")
+            plt.title(f"Plane : {p}")
             plt.tight_layout()
             plt.savefig(f"{args.outdir}/scatter_{p}.png", dpi=400)
+            plt.close()
 
     if args.plot == "validation":        
         data = pd.DataFrame(TPDataFrame())
@@ -195,6 +146,6 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--out-directory", dest="outdir", type=str, default="plot", help="output file directory to store plots")
     parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="print record and TP information")
     parser.add_argument("-p", "--plot", dest="plot", choices=["validation", "gif", "scatter"], help="create plots of TP data")
-    parser.add_argument("-t", "--timeslice", dest="timeslice", type=int, default=-1, help="timeslice number to plot")
+    parser.add_argument("-t", "--timeslice", dest="timeslice", type=int, default=-1, help="timeslice to plot")
     args = parser.parse_args()
     main(args)
